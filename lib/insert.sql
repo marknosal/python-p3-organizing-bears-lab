6745e08d44efbc8c514173aefbcc56ea9bd5f26c
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-15 20:42:26 -0500

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 6, 'M', 'Brown', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Light Brown', 'Loud', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'W', 'Black', 'Quiet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'Brown', 'Strict', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'W', 'Red', 'Sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'M', 'Gray', 'Grouchy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'W', 'Light Brown', 'Nice', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 99, 'M', 'Unknown', 'Mysterious', 0);
